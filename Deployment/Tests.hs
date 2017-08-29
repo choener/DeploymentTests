c199e543ac61196b15fc321170827863250274c0
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2017-08-30 00:44:00 +0200

    deployment test library mostly ok

--- Deployment/Tests.hs
+++ Deployment/Tests.hs
@@ -1,7 +1,10 @@
 
 module Deployment.Tests where
 
+import Data.FileEmbed
 
+
+testData = $(embedFile "data/data.dat")
 
 test = "test"
 
--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,16 @@
 
 module Main where
 
+import Deployment.Tests
 
 
 main :: IO ()
 main = do
+  print "string"
+  print test
+  print "embedded"
+  print testData
+  print "readFile"
+  readFile "data/data.dat" >>= print
   return ()
 
