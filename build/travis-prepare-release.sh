7f02659bf350724421c9815c327f5286644dbe11
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2017-08-30 23:50:16 +0200

    build dir

--- build/travis-prepare-release.sh
+++ build/travis-prepare-release.sh
@@ -2,13 +2,15 @@
 
 # prepare release files
 P=`pwd`
-D=`basename ${P}`
-rm -fr $D
-mkdir $D
+B=`basename ${P}`
+D="build/$B"
+rm -fr build
+mkdir -p $D
 # copy executable(s)
 cp `find dist-newstyle -executable -type f -name DeploymentTests` $D
 # copy data
 cp -r data $D
 # prepare tar.gz
-tar czf $D.tgz $D
+cd build
+tar czf $B.tgz $B
 
